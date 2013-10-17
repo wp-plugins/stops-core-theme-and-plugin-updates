5364da85b301eaa0f858f6896bba2ca994f717d3
kidsguide <kidsguide@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-17 19:45:14 +0000

    Updated
    git-svn-id: https://plugins.svn.wordpress.org/stops-core-theme-and-plugin-updates/trunk@789471 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Function.php
+++ Function.php
@@ -5,7 +5,7 @@
  * @package Disable All Updates
  * @author Websiteguy
 
- * @version 0.6
+ * @version 0.7
 */
 
 /*
@@ -13,7 +13,7 @@
 Plugin Name: Disable All Updates
 
 Plugin URI: http://wordpress.org/plugins/stops-core-theme-and-plugin-updates/
-Version: 0.6
+Version: 0.7
 Description: A Simple Wordpress Plugin That Deletes All Updating of Plugins, Themes, and Even The Wordpress Core.
 
 Author: <a href="http://profiles.wordpress.org/kidsguide">Websiteguy</a>
