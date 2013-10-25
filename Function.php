7f87b5ce562ae558ac126a5364670002404f175a
kidsguide <kidsguide@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-25 13:43:11 +0000

    Updated
    git-svn-id: https://plugins.svn.wordpress.org/stops-core-theme-and-plugin-updates/trunk@793616 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Function.php
+++ Function.php
@@ -16,7 +16,7 @@ Plugin URI: http://wordpress.org/plugins/stops-core-theme-and-plugin-updates/
 Version: 0.7
 Description: A Simple Wordpress Plugin That Deletes All Updating of Plugins, Themes, and Even The Wordpress Core.
 
-Author: 
+Author: <a href="http://profiles.wordpress.org/kidsguide">Websiteguy</a>
 Author URL: http://profiles.wordpress.org/kidsguide
 Compatible with WordPress 2.3+.
 
