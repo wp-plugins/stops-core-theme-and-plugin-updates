995d6704acfb08072b7ce8b28975888e14598690
kidsguide <kidsguide@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-19 17:27:31 +0000

    updated to version 1.4
    git-svn-id: https://plugins.svn.wordpress.org/stops-core-theme-and-plugin-updates/tags/1.3@825672 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Function.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * @package Disable All Updates
+ * @author Websiteguy
+ * @version 1.3
+*/
+/*
+Plugin Name: Disable All Updates
+Plugin URI: http://wordpress.org/plugins/stops-core-theme-and-plugin-updates/
+Version: 1.3
+Description: A simple WordPress plugin that disables all the updating of plugins, themes, and the WordPress core. Their is no setup for this plugin.
+Author: <a href="http://profiles.wordpress.org/kidsguide">Websiteguy</a>
+Author URL: http://profiles.wordpress.org/kidsguide
+Compatible with WordPress 2.3+.
+*/
+/*
+Copyright 2013 Websiteguy (email : mpsparrow@cogeco.ca)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// Disable Plugin Updates
+remove_action( 'load-update-core.php', 'wp_update_plugins' );
+add_filter( 'pre_site_transient_update_plugins', create_function( '$a', "return null;" ) );
+
+// Disable Theme Updates
+remove_action( 'load-update-core.php', 'wp_update_themes' );
+add_filter( 'pre_site_transient_update_themes', create_function( '$a', "return null;" ) );
+
+// Disable Core Updates
+remove_action( 'load-update-core.php', 'wp_update_core' );
+add_filter( 'pre_site_transient_update_core', create_function( '$a', "return null;" ) );
+
+// Remove Updates Menu
+add_action('admin_menu', 'remove_menus', 102);
+function remove_menus() {
+	global $submenu;
+	remove_submenu_page ( 'index.php', 'update-core.php' );
+}
+
+// Hides Update Notices in Dashboard
+add_action('admin_menu','hide_admin_notices');
+function hide_admin_notices() {
+    remove_action( 'admin_notices', 'update_nag', 3 );
+}
+
+// Turn off Automatic Updates in WordPress
+define( 'Automatic_Updater_Disabled', true );
+
+// Remove Update E-mails (Only works with some plugins)
+// Core E-mails
+apply_filters( 'auto_core_update_send_email', false, $type, $core_update, $result );
+
+// Plugin E-mails
+apply_filters( 'auto_plugin_update_send_email', false, $type, $plugin_update, $result );
+
+// Theme E-mails
+apply_filters( 'auto_theme_update_send_email', false, $type, $theme_update, $result );
+
+// Remove Files From WordPress
+	function admin_init() {
+		if ( !function_exists("remove_action") ) return;
+
+		// Disable Plugin Updates
+		remove_action( 'load-plugins.php', 'wp_update_plugins' );
+		remove_action( 'load-update.php', 'wp_update_plugins' );
+		remove_action( 'admin_init', '_maybe_update_plugins' );
+		remove_action( 'wp_update_plugins', 'wp_update_plugins' );
+		wp_clear_scheduled_hook( 'wp_update_plugins' );
+		
+		remove_action( 'load-update-core.php', 'wp_update_plugins' );
+		wp_clear_scheduled_hook( 'wp_update_plugins' );	
+
+		// Disable Theme Updates
+		remove_action( 'load-themes.php', 'wp_update_themes' );
+		remove_action( 'load-update.php', 'wp_update_themes' );
+		remove_action( 'admin_init', '_maybe_update_themes' );
+		remove_action( 'wp_update_themes', 'wp_update_themes' );
+		wp_clear_scheduled_hook( 'wp_update_themes' );
+		
+		remove_action( 'load-update-core.php', 'wp_update_themes' );
+		wp_clear_scheduled_hook( 'wp_update_themes' );
+
+		// Disable Core Updates
+		remove_action( 'wp_version_check', 'wp_version_check' );
+		remove_action( 'admin_init', '_maybe_update_core' );
+		wp_clear_scheduled_hook( 'wp_version_check' );
+		
+		wp_clear_scheduled_hook( 'wp_version_check' );
+	}
+
+// Remove Updates Agian (just in case)
+add_filter( 'pre_site_transient_update_plugins', create_function( '$a', "return null;" ) );
+
+add_action( 'init', create_function( '$a', "remove_action( 'init', 'wp_version_check' );" ), 2 );
+add_filter( 'pre_option_update_core', create_function( '$a', "return null;" ) );
+
+remove_action( 'wp_version_check', 'wp_version_check' );
+remove_action( 'admin_init', '_maybe_update_core' );
+add_filter( 'pre_transient_update_core', create_function( '$a', "return null;" ) );
+
+add_filter( 'pre_site_transient_update_core', create_function( '$a', "return null;" ) );
+
+remove_action( 'load-themes.php', 'wp_update_themes' );
+remove_action( 'load-update.php', 'wp_update_themes' );
+remove_action( 'admin_init', '_maybe_update_themes' );
+remove_action( 'wp_update_themes', 'wp_update_themes' );
+add_filter( 'pre_transient_update_themes', create_function( '$a', "return null;" ) );
+
+remove_action( 'load-update-core.php', 'wp_update_themes' );
+add_filter( 'pre_site_transient_update_themes', create_function( '$a', "return null;" ) );
+
+add_action( 'admin_menu', create_function( '$a', "remove_action( 'load-plugins.php', 'wp_update_plugins' );") );
+	
+add_action( 'admin_init', create_function( '$a', "remove_action( 'admin_init', 'wp_update_plugins' );"), 2 );
+add_action( 'init', create_function( '$a', "remove_action( 'init', 'wp_update_plugins' );"), 2 );
+add_filter( 'pre_option_update_plugins', create_function( '$a', "return null;" ) );
+
+remove_action( 'load-plugins.php', 'wp_update_plugins' );
+remove_action( 'load-update.php', 'wp_update_plugins' );
+remove_action( 'admin_init', '_maybe_update_plugins' );
+remove_action( 'wp_update_plugins', 'wp_update_plugins' );
+add_filter( 'pre_transient_update_plugins', create_function( '$a', "return null;" ) );
+
+remove_action( 'load-update-core.php', 'wp_update_plugins' );
+add_filter( 'pre_site_transient_update_plugins', create_function( '$a', "return null;" ) );
